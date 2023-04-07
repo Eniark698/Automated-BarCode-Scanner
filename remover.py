6ff0fe4a4e194a864a8d37db07ac57223a914767
Nazarii Mozol <eniark698@gmail.com>
2023-04-07 16:53:14 +0300

    remover.py takes day parameter from config.json

--- remover.py
+++ remover.py
@@ -2,6 +2,7 @@ def remove():
     from os import listdir
     from datetime import datetime
     from shutil import rmtree
+    import json
 
     def days_between(d1, d2):
         d1 = datetime.strptime(d1, "%Y-%m-%d")
@@ -12,9 +13,25 @@ def remove():
 
     path='/home/parallels/Downloads/'
 
+
+    with open(path + 'config.json') as json_file:
+        data = json.load(json_file)
+        try:
+            days=data["days_to_remove"]
+        except Exception as err:
+            days=100
+            f=open(path + 'log_conf.txt', 'a')
+            f.write('--\n')
+            f.write(str(Exception))
+            f.write('\n')
+            f.write(str(err))
+            f.write('occurred on ' + str(datetime.now()))
+            f.write('\n--\n\n\n')
+            f.close()
+
     list=listdir(path + ('Processed/done/'))
     for i in list:
-        if abs(days_between(now_date,i))>100:
+        if abs(days_between(now_date,i))>days:
             rmtree(path + 'Processed/done/{}'.format(i))
 
 remove()
