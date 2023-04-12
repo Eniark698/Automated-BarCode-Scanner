e9e35c4a210a3a5816697685a7bdf9d960161641
Nazarii Mozol <eniark698@gmail.com>
2023-04-12 15:38:33 +0300

    v 2.0.1
    betatest

--- remover.py
+++ remover.py
@@ -3,6 +3,7 @@ def remove():
     from datetime import datetime
     from shutil import rmtree
     import json
+    import exifread
 
     def days_between(d1, d2):
         d1 = datetime.strptime(d1, "%Y-%m-%d")
@@ -10,9 +11,27 @@ def remove():
         return abs((d2 - d1).days)
     now_date=datetime.today().strftime('%Y-%m-%d')
 
-    global path
-    path='F:\project/'
 
+    path='C:/scan_proj/'
+
+
+    with open(path + 'config.json') as json_file:
+            data = json.load(json_file)
+            try:
+                donefolder=data["path to placement of done`s folder"]
+                logsfolder=data["path to placement of log`s folder"]
+            except Exception as err:
+                donefolder="C:/scan_proj/done/"
+                logsfolder="C:/scan_proj/logs/"
+
+                f=open(logsfolder + 'log_conf_rem.txt', 'a')
+                f.write('--\n')
+                f.write(str(Exception))
+                f.write('\n')
+                f.write(str(err))
+                f.write('occurred on ' + str(datetime.now()))
+                f.write('\n--\n\n\n')
+                f.close()
 
     with open(path + 'config.json') as json_file:
         data = json.load(json_file)
@@ -20,7 +39,7 @@ def remove():
             days=data["days_to_remove"]
         except Exception as err:
             days=100
-            f=open(path + 'log_conf.txt', 'a')
+            f=open(logsfolder + 'log_conf_rem.txt', 'a')
             f.write('--\n')
             f.write(str(Exception))
             f.write('\n')
@@ -29,9 +48,14 @@ def remove():
             f.write('\n--\n\n\n')
             f.close()
 
-    list=listdir(path + ('Processed/done/'))
+
+    list=listdir(donefolder)
     for i in list:
-        if abs(days_between(now_date,i))>=days:
-            rmtree(path + 'Processed/done/{}'.format(i))
+        with open(donefolder + i, 'rb') as fh:
+            tags = exifread.process_file(fh, stop_tag="EXIF DateTimeOriginal")
+            dateTaken = tags["EXIF DateTimeOriginal"]
+            dateTaken = dateTaken.strftime('%Y-%m-%d')
+            if abs(days_between(now_date,dateTaken))>=days:
+                rmtree(donefolder + str(i))
 
 remove()
