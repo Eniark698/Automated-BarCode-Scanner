6c2cf3cd89e708a6f18cfafb16967e4b139cbc80
Nazarii Mozol <nmozol@berta.com.ua>
2023-04-24 12:16:31 +0300

    v3.0.3
    added comments

--- scanner.py
+++ scanner.py
@@ -1,5 +1,6 @@
 def scan(scanfolder, donefolder,oldfolder,problemfolder,logsfolder):
 
+    #exit from another instance, if two times script was started
     from tendo import singleton
     from sys import exit
     try:
@@ -20,6 +21,7 @@ def scan(scanfolder, donefolder,oldfolder,problemfolder,logsfolder):
     from traceback import format_exc
 
 
+    #connect to psql, create table if not exists
     import psycopg2
     con = psycopg2.connect(
         host="localhost",
@@ -38,39 +40,51 @@ def scan(scanfolder, donefolder,oldfolder,problemfolder,logsfolder):
         ,direction varchar(1));""")
     con.commit()
      
+    #unique seed for random lib 
     seed(hash(str(datetime.now())))
 
     
 
+    #get list of files in scanfoler
     list=listdir(scanfolder)
     total, j, Mem = 0 , 0 , []
    
+
+   #procces that files
     try:
         for i in list:
             start_time = time()
 
+
+            #if file can not be opened(not a photo), move to problem folder
             try:
                 image = Image.open(scanfolder + str(i))
             except:
                 move(scanfolder + str(i), problemfolder + str(i))
                 j+=1
                 continue
-
+            
+            #get format and size of photo
             format=image.format
             size=stat(scanfolder + str(i)).st_size
             codes=[]
 
+
+            #get all barcodes from photo
             decoded_objects = pyzbar.decode(image)
             for obj in decoded_objects:
                 answer=obj.data.decode()
                 typecode=obj.type
                 codes.append((answer,typecode))
 
+            #if no codes was detected, move to problem folder
             if codes == []:
                 move(scanfolder + str(i), problemfolder + str(i))
                 j+=1
                 continue
+            
 
+            #iterator thought all barcodes
             for (answer,typecode) in codes:
                 answer=str(answer)
                 r=random()
@@ -78,13 +92,16 @@ def scan(scanfolder, donefolder,oldfolder,problemfolder,logsfolder):
                 h=h[:3]
                 direction=answer[-1]
                 direction=direction.lower()
-                
+                #if barcode in type code128, then last symbol is direction of sales
 
-                   
+                #for code128, with correct ending 
                 if  typecode=='CODE128' and (direction in ['F', 'f', 'N', 'n']):
                     answer=answer[:-2]
                     name = answer + ',' + h + '.' + format
                     
+
+                    #try to make folder with code128 name
+                    #if folder have been already exists, get size of all files in it, if matching then skip this photo
                     try:
                         mkdir(donefolder + direction + '/' + answer)
                     except:
@@ -98,8 +115,12 @@ def scan(scanfolder, donefolder,oldfolder,problemfolder,logsfolder):
                         else:
                             pass
                 
+
                     datn=str(datetime.now())
 
+
+
+                    #insert all info about scan in DB
                     cur.execute("""insert into scantable values ('{}','{}','{}','{}',{},'{}','{}');""".format(
                                     name
                                     ,answer
@@ -108,24 +129,38 @@ def scan(scanfolder, donefolder,oldfolder,problemfolder,logsfolder):
                                     ,str(size)
                                     ,typecode
                                     ,direction))
+                    
+                    #copy scan into folder
                     copy(scanfolder + str(i)
                         ,donefolder + direction + '/' + answer + '/' + name)
+                    
+                    #commit inserting into DB
                     con.commit()
 
+
+                #all correct code39 and ean13 into oldefolder
                 elif len(answer)==13 and (typecode== 'EAN13' or typecode== 'CODE39'):
                     name = answer + ',' + h + '.' + format
                     copy(scanfolder + str(i)
                         ,oldfolder + name)
-                 
+                
+
+
+                #any else into problem folder
                 else:
                     copy(scanfolder + str(i), problemfolder + str(i))
             remove(scanfolder + str(i))        
             
+
+            #get info about time and memory in current itaration
             total+= time() - start_time
             process = Process(getpid())
             Mem.append(process.memory_info().rss/1024/1024)
             j+=1
+    
 
+
+    #if error, write to log file
     except:
         f=open(logsfolder + 'log_scan.txt', 'a')
         f.write('----------------------------------------\n')
@@ -136,6 +171,7 @@ def scan(scanfolder, donefolder,oldfolder,problemfolder,logsfolder):
         f.close()
         
 
+    #get info about loop
     else:
         try:
             max=0
